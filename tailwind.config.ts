848e70cebf99db1908df8a2132542ca190407c60
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-07-26 19:08:04 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        // 25px / 16 = 1.5625rem
+        'page-margin': '1.5625rem',
+        // 24px / 16 = 1.5rem (for the grid gutter)
+        'page-gutter': '1.5rem',
+      },
+    },
   },
   plugins: [],
 };
