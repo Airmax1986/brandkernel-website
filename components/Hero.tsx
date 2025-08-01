2e0e487c74e3de531c2d3b13e6ac133b1bd1506e
Airmax <126975078+Airmax1986@users.noreply.github.com>
2025-08-01 09:10:18 +0200

    Update Hero import to use cleaner chat index path

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@
 import { motion } from "framer-motion";
 import GridContainer from "./GridContainer";
 import CtaButton from "./CtaButton";
-import { Chatbot } from "./chat/Chatbot";
+import { Chatbot } from "./chat";
 
 export default function Hero() {
   return (
